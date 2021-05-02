f659697a803a389230a10fda598bfb287230ae26
nguy2869 <75713720+nguy2869@users.noreply.github.com>
2021-05-01 22:36:39 -0500

    Add files via upload

---
+++ Python-api-challenge/VacationPy/config.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "2ec81bb0a966b4c70aef34433f76fb64"
+
+
+# Google API Key
+g_key = "AIzaSyABA1c9qkujL2N8oME3xlTBM7aEKZyNPyE"
---
+++ Python-api-challenge/WeatherPy/config.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "2ec81bb0a966b4c70aef34433f76fb64"
+
+
+# Google API Key
+g_key = "AIzaSyDXjgub22iAO6piwsknr1P4InmA9O2SAfw"
